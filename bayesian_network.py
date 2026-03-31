52bd1b507c1a0f997ed890b868cbd3aa9df18a7d
Sahithrv360 <rvsahithreddy@gmail.com>
2026-03-31 10:10:14 +0530

    complete code for predicting for rain,grass,sprinkler variables

--- bayesian_network.py
+++ bayesian_network.py
@@ -1,3 +1,28 @@
-from pgmpy.models import BayesianNetwork
+from pgmpy.models import DiscreteBayesianNetwork
 from pgmpy.factors.discrete import TabularCPD
 from pgmpy.inference import VariableElimination
+
+model = DiscreteBayesianNetwork([('Rain','Sprinkler'),
+                        ('Rain','WetGrass'),
+                        ('Sprinkler','WetGrass')]
+                    )
+cpd_rain = TabularCPD(  variable='Rain',variable_card=2,values=[[0.8],[0.2]] )
+
+cpd_sprinkler = TabularCPD( variable='Sprinkler',variable_card=2,
+                            values=([0.99, 0.6],
+                                   [0.01, 0.4]),
+                            evidence=['Rain'],
+                            evidence_card=[2] )
+
+cpd_wetgrass = TabularCPD(variable='WetGrass',variable_card=2,
+                          values=[[0.01, 0.1, 0.1, 0.01],
+                                  [0.99, 0.9, 0.9, 0.99]],
+                          evidence=['Rain','Sprinkler'],
+                          evidence_card=[2,2]
+                          )
+model.add_cpds(cpd_rain,cpd_sprinkler,cpd_wetgrass)
+print(model.check_model())
+
+infer = VariableElimination(model)
+query_result = infer.query(['Rain'],evidence={'WetGrass' : 0})
+print(query_result)
