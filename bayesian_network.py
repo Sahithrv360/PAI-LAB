5b209045459784069d798dbc0b44d864e7f81f26
Sahithrv360 <rvsahithreddy@gmail.com>
2026-03-31 11:54:10 +0530

    refactored code reduced class names

--- bayesian_network.py
+++ bayesian_network.py
@@ -1,20 +1,20 @@
-from pgmpy.models import DiscreteBayesianNetwork
-from pgmpy.factors.discrete import TabularCPD
-from pgmpy.inference import VariableElimination
+from pgmpy.models import DiscreteBayesianNetwork as DBN
+from pgmpy.factors.discrete import TabularCPD as TCPD
+from pgmpy.inference import VariableElimination as VE
 
-model = DiscreteBayesianNetwork([('Rain','Sprinkler'),
+model = DBN([('Rain','Sprinkler'),
                         ('Rain','WetGrass'),
                         ('Sprinkler','WetGrass')]
                     )
-cpd_rain = TabularCPD(  variable='Rain',variable_card=2,values=[[0.8],[0.2]] )
+cpd_rain = TCPD(  variable='Rain',variable_card=2,values=[[0.8],[0.2]] )
 
-cpd_sprinkler = TabularCPD( variable='Sprinkler',variable_card=2,
+cpd_sprinkler = TCPD( variable='Sprinkler',variable_card=2,
                             values=([0.99, 0.6],
                                    [0.01, 0.4]),
                             evidence=['Rain'],
                             evidence_card=[2] )
 
-cpd_wetgrass = TabularCPD(variable='WetGrass',variable_card=2,
+cpd_wetgrass = TCPD(variable='WetGrass',variable_card=2,
                           values=[[0.01, 0.1, 0.1, 0.01],
                                   [0.99, 0.9, 0.9, 0.99]],
                           evidence=['Rain','Sprinkler'],
@@ -23,6 +23,6 @@ cpd_wetgrass = TabularCPD(variable='WetGrass',variable_card=2,
 model.add_cpds(cpd_rain,cpd_sprinkler,cpd_wetgrass)
 print(model.check_model())
 
-infer = VariableElimination(model)
+infer = VE(model)
 query_result = infer.query(['Rain'],evidence={'WetGrass' : 0})
 print(query_result)
