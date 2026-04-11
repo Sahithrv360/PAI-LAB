82988d680801a249d17305a691b53d23b66159f1
Sahithrv360 <rvsahithreddy@gmail.com>
2026-04-11 10:40:57 +0530

    updated the code to print the values under each case

--- Hidden_Markov_model.py
+++ Hidden_Markov_model.py
@@ -39,6 +39,7 @@ def forward_algorithm(states,observations,ini_prob,transition_probabilities,emis
     for state in states:
         f0[state] = ini_prob[state]*emission_probabilities[state][observation_sequence[0]]
     forward.append(f0)
+    print(f0)
     for t in range(1,len(observation_sequence)):
         ft = {}
         for current_state in states:
@@ -47,10 +48,12 @@ def forward_algorithm(states,observations,ini_prob,transition_probabilities,emis
                 prob += forward[t-1][previous_state] * transition_probabilities[previous_state][current_state]
             ft[current_state] = prob * emission_probabilities[current_state][observation_sequence[t]]
         forward.append(ft)
-
+    for t, fwd in enumerate(forward):
+        print(f"\nTime step {t}:")
+        for state, value in fwd.items():
+            print(f"{state} : {value}")
     total_probability = sum(forward[-1][state] for state in states)
     return total_probability
-
 result = forward_algorithm(states,observations,ini_prob,transition_probabilities,emission_probabilities,
                            observation_sequence)
 
